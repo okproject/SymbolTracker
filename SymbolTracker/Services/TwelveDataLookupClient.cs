8a35206cfa1ae9f1f2fdf16e0e9f6bbd1b02ba8a
Rasih CAGLAYAN <rasihcaglayan@gmail.com>
2021-06-18 09:11:35 +0300

    adds twelveDataLookup client

--- SymbolTracker/Services/TwelveDataLookupClient.cs
+++ SymbolTracker/Services/TwelveDataLookupClient.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace SymbolTracker.Services
 {
-    public class TwelveDataLookupClient:ISymbolLookupClient
+    public class TwelveDataLookupClient : ISymbolLookupClient
     {
+        //25556349db984abd8e6a7a6062a733b4
+        private string _apiUrl { get; set; } =
+            "https://api.twelvedata.com/time_series?symbol=AAPL,MSFT,EUR/USD,SBUX,NKE&interval=1min&apikey=demo&source=docs";
+
         private readonly HttpClient _httpClient;
 
         public TwelveDataLookupClient(HttpClient httpClient)
@@ -12,9 +20,25 @@ namespace SymbolTracker.Services
             _httpClient = httpClient;
         }
 
-        public JObject GetTimeSeries()
+        public async Task<List<StockSymbol>> GetTimeSeries()
         {
-            return new JObject();
+            List<StockSymbol> stockSymbols = new List<StockSymbol>();
+            var httpResponse = await _httpClient.GetAsync(_apiUrl);
+            var str = await httpResponse.Content.ReadAsStringAsync();
+            var result = JObject.Parse(str);
+
+            foreach (var jo in result)
+            {
+                var item = result[jo.Key];
+                var res = new StockSymbol();
+                res.Symbol = jo.Key;
+                res.Meta = JsonConvert.DeserializeObject<Meta>(item["meta"].ToString());
+                res.Status = item["status"].ToString();
+                res.Values = JsonConvert.DeserializeObject<List<Value>>(item["values"].ToString());
+                stockSymbols.Add(res);
+            }
+
+            return stockSymbols;
         }
     }
 }
