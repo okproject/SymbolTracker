8a431a5b9eb02c48efb97559565c72887ff67ffc
Rasih CAGLAYAN <rasihcaglayan@gmail.com>
2021-06-18 05:21:48 +0300

    update stockServiceClient to inject httpClient

--- SymbolTracker/Services/StockServiceClient.cs
+++ SymbolTracker/Services/StockServiceClient.cs
@@ -1,9 +1,17 @@
+using System.Net.Http;
 using Newtonsoft.Json.Linq;
 
 namespace SymbolTracker.Services
 {
     public class StockServiceClient:IStockServiceClient
     {
+        private readonly HttpClient _httpClient;
+
+        public StockServiceClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public JObject GetTimeSeries()
         {
             throw new System.NotImplementedException();
